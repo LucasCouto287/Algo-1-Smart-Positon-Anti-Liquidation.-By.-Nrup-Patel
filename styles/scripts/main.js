14d325c2ea7ae87aa642e938e02b370ba1433342
Nrup R Patel <nruppatel133@gmail.com>
2020-01-08 18:54:26 -0500

    Update main.js

--- styles/scripts/main.js
+++ styles/scripts/main.js
@@ -1,1 +1,17 @@
+let app = document.getElementById('app');
+
+let menu = document.getElementById('menu1');
+let menu2 = document.getElementById('menu2');
+
+let row1 = document.getElementById('row1');
+let row2 = document.getElementById('row2');
+let row3 = document.getElementById('row3');
+let row4 = document.getElementById('row4');
+
+let tabsMain = {
+    "BitMEX": document.getElementById('tab-BitMEX'),
+    "Deribit": document.getElementById('tab-Deribit'),
+    "Guide": document.getElementById('tab-Guide'),
+    "Traders": document.getElementById('tab-Traders'),
+};
 
